5240ca88eba5b96410f72cee6632b1d5141ae91e
Vova <awdawfaw@gmail.com>
2020-11-05 19:23:18 +0300

    models

--- src/dockerlesson/firstapp/models.py
+++ src/dockerlesson/firstapp/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class A(models.Model):
+    title = models.CharField(max_length=100)
+    b = models.ForeignKey(
+        'B',
+        on_delete=models.CASCADE,
+        related_name='name_b',
+        null=True,
+        blank=True
+    )
+
+
+class B(models.Model):
+    title = models.CharField(max_length=100)
+    a = models.ForeignKey(A, on_delete=models.CASCADE, related_name='name_a')
+
+class C(models.Model):
+    title = models.CharField(max_length=100)
+
+class D(models.Model):
+    title = models.CharField(max_length=100)
+    m2m = models.ManyToManyField(C)
+
+
